c40deee1a689252434d28636fcc7060242a32ee0
Unadia16 <unadiaaarumexander@gmail.com>
2025-03-21 21:24:20 +0800

    Add files via upload

---
+++ CS1B_MTExam_UNADIA/classSubjects.js
@@ -0,0 +1,10 @@
+//Coding problem 2
+//UNADIA AARUME XANDER BSCS-1B 
+let subjectTitle = prompt("Enter your subject title: ");//Prompting the user to enter their subject title.
+let classSchedule = prompt("Enter your class schedule (Time, Days):");//Prompting the user to enter their class schedule.
+let classroom = prompt("Enter classroom number or name:");//Prompting the user to enter their classroom number or name.
+let classInstructor = prompt("Enter the name of your instructor:");//Prompting the user to enter their instructor's name.
+let studentName = prompt("Enter your name my friend:");//Prompting the user to enter his/her name.
+
+//Logging it to the console.
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`); 

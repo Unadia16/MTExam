0c6c0cfc760a55b83cdb91ef033d88534aca289f
Unadia16 <unadiaaarumexander@gmail.com>
2025-03-21 21:22:49 +0800

    Add files via upload

---
+++ CS1B_MTExam_UNADIA/userProfile.js
@@ -0,0 +1,13 @@
+//Coding Problem 1
+//UNADIA AARUME XANDER BSCS-1B 
+let fullName = prompt("Enter your name my friend:");//Prompting the user to enter their full name.
+let age = prompt("Enter your age my friend:");//Prompting the user to enter their age.
+let favoriteNumber = prompt("Enter your favorite number my friend:");//Prompting the user to enter their favorite number.
+let favoriteColor = prompt("Enter your favorite color my friend:");//Prompting the user to enter their favorite color.
+
+//Using the variables in logging the collected data in the console.
+console.log("Full name:", fullName);
+console.log("Age:", age);
+console.log("Favorite number:", favoriteNumber);
+console.log("Favorite color:", favoriteColor);
+
